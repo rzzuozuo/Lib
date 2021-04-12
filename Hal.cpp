7e8d0c2f470d10146f62cb78ef78b76d9278a4e7
Allen Wang <326727955@qq.com>
2021-04-12 15:36:51 +0800

    【hal】【init】

---
+++ Hal.cpp
@@ -0,0 +1,18 @@
+/*
+ * Hal.cpp
+ *
+ *  Created on: 2021年4月12日
+ *      Author: Administrator
+ */
+
+#include "Hal.hpp"
+#include "main.h"
+
+Hal::Hal() {
+	HAL_Init();
+}
+
+Hal::~Hal() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Hal.hpp
@@ -0,0 +1,17 @@
+/*
+ * Hal.h
+ *
+ *  Created on: 2021年4月12日
+ *      Author: Administrator
+ */
+
+#ifndef APPLICATION_LIB_HAL_H_
+#define APPLICATION_LIB_HAL_H_
+
+class Hal {
+public:
+	Hal();
+	virtual ~Hal();
+};
+
+#endif /* APPLICATION_LIB_HAL_H_ */
