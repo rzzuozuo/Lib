1204714d4b8b14d682b17313c256c3fe82c58ab9
Allen Wang <326727955@qq.com>
2021-04-16 08:40:42 +0800

    update can

---
+++ Can.cpp
@@ -0,0 +1,18 @@
+/*
+ * Can.cpp
+ *
+ *  Created on: Apr 14, 2021
+ *      Author: Administrator
+ */
+
+#include "Can.h"
+
+Can::Can() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Can::~Can() {
+	// TODO Auto-generated destructor stub
+}
+
