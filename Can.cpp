8c8d4b2f4d4958290720c89424397c15acb64b88
Allen Wang <326727955@qq.com>
2021-04-27 17:33:59 +0800

    fix a bug about can init

--- Can.cpp
+++ Can.cpp
@@ -12,17 +12,23 @@ Can::Can(CAN_HandleTypeDef &hcan):hcan(hcan){
 }
 
 Can::~Can() {
+	HAL_CAN_DeInit(&hcan);
 }
 
 Can::StatusTypeDef Can::init(uint32_t Prescaler,uint32_t TimeSeg1, uint32_t TimeSeg2, uint32_t SyncJumpWidth){
 	StatusTypeDef state;
 
+	assert_param(Prescaler > 0);
+	assert_param(TimeSeg1 > 0);
+	assert_param(TimeSeg2 > 0);
+	assert_param(SyncJumpWidth > 0);
+
 	hcan.Instance = CAN1;
-	hcan.Init.Prescaler = 8;
+	hcan.Init.Prescaler = Prescaler;
 	hcan.Init.Mode = CAN_MODE_NORMAL;
-	hcan.Init.SyncJumpWidth = CAN_SJW_1TQ;
-	hcan.Init.TimeSeg1 = CAN_BS1_2TQ;
-	hcan.Init.TimeSeg2 = CAN_BS2_1TQ;
+	hcan.Init.SyncJumpWidth = ((SyncJumpWidth-1) << CAN_BTR_SJW_Pos) & CAN_BTR_SJW_Msk;
+	hcan.Init.TimeSeg1 = ((TimeSeg1-1) << CAN_BTR_TS1_Pos) & CAN_BTR_TS1_Msk;
+	hcan.Init.TimeSeg2 = ((TimeSeg2-1) << CAN_BTR_TS2_Pos) & CAN_BTR_TS2_Msk;;
 	hcan.Init.TimeTriggeredMode = DISABLE;
 	hcan.Init.AutoBusOff = ENABLE;
 	hcan.Init.AutoWakeUp = DISABLE;
@@ -68,7 +74,7 @@ Can::StatusTypeDef Can::start(){
 #if USE_HAL_CAN_REGISTER_CALLBACKS == 1
 	std::function<void(CAN_HandleTypeDef *)> callabck = std::bind(&Can::Fifo0MsgPendingCallback,this,std::placeholders::_1);
 
-	callabck(&this->hcan);
+	//callabck(&this->hcan);
 
 	state =  (StatusTypeDef)HAL_CAN_RegisterCallback(&hcan, HAL_CAN_RX_FIFO0_MSG_PENDING_CB_ID, (Fifo0MsgPendingCallback_t) &callabck);
 
