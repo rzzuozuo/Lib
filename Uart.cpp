154caaadd407bc558869eb760854b0049b530822
Allen Wang <326727955@qq.com>
2021-04-29 17:07:02 +0800

    add uart

---
+++ Uart.cpp
@@ -0,0 +1,63 @@
+/*
+ * Uart.cpp
+ *
+ *  Created on: Apr 29, 2021
+ *      Author: Administrator
+ */
+
+#include <Uart.hpp>
+
+#define MAX_UART_NUM 	8
+static Uart* handles[MAX_UART_NUM] = {0};
+
+Uart::Uart(UART_HandleTypeDef &huart):huart(huart) {
+	for(int i = 0; i < MAX_UART_NUM ;i++){
+		if(handles[i] == NULL){
+			handles[i] = this;
+			break;
+		}
+	}
+}
+
+Uart::~Uart() {
+	HAL_UART_DeInit(&huart);
+}
+
+Uart::StatusTypeDef Uart::init(USART_TypeDef * uart,uint32_t baudrate, WordLength wordLength , StopBits stopBits, Parity parity){
+	StatusTypeDef status;
+
+	huart.Instance = uart;
+	huart.Init.BaudRate = baudrate;
+	huart.Init.WordLength = wordLength;
+	huart.Init.StopBits = stopBits;
+	huart.Init.Parity = parity;
+	huart.Init.Mode = UART_MODE_TX_RX;
+	huart.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+	huart.Init.OverSampling = UART_OVERSAMPLING_16;
+
+	status = (StatusTypeDef)HAL_UART_Init(&huart);
+	if ( status!= OK)
+	{
+		error();
+	}
+	return status;
+}
+
+
+Uart::StatusTypeDef Uart::receive(uint8_t* data,int size){
+	__HAL_UART_CLEAR_OREFLAG(&huart);
+	return (StatusTypeDef)HAL_UART_Receive_IT(&huart, data , size);
+}
+
+Uart::StatusTypeDef  Uart::transmit(uint8_t* data, int size){
+	return (StatusTypeDef)HAL_UART_Transmit_IT(&huart, data , size);
+}
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart){
+	for(int i = 0; (handles[i] != NULL)&& (i < MAX_UART_NUM); ++i){
+		if(handles[i]->huart.Instance == huart->Instance){
+			handles[i]->rxCpltCallback();
+		}
+	}
+}
+
---
+++ Uart.hpp
@@ -0,0 +1,51 @@
+/*
+ * Uart.h
+ *
+ *  Created on: Apr 29, 2021
+ *      Author: Administrator
+ */
+
+#ifndef UART_HPP_
+#define UART_HPP_
+
+#include <Hal.hpp>
+
+class Uart: public Hal {
+	friend void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart);
+public:
+	enum WordLength:unsigned int{
+		BYTES_8 = UART_WORDLENGTH_8B,
+		BYTES_9 = UART_WORDLENGTH_9B,
+	};
+
+	enum StopBits:unsigned int{
+		STOP_1 = UART_STOPBITS_1,
+		STOP_2 = UART_STOPBITS_2,
+	};
+
+	enum Parity:unsigned int{
+		NONE = UART_PARITY_NONE,
+		EVEN = UART_PARITY_EVEN,
+		ODD = UART_PARITY_ODD,
+	};
+
+	Uart(UART_HandleTypeDef &huart);
+	virtual ~Uart();
+
+	StatusTypeDef init(USART_TypeDef * uart = USART1, uint32_t baudrate = 115200, WordLength wordLength = BYTES_8, StopBits stopBits = STOP_1, Parity parity = NONE);
+	virtual void init(){
+
+	}
+	StatusTypeDef receive(uint8_t* data,int size);
+	StatusTypeDef transmit(uint8_t* data, int size);
+	virtual void rxCpltCallback(){
+
+	}
+private:
+	UART_HandleTypeDef &huart;
+	virtual void error(){
+
+	}
+};
+
+#endif /* UART_HPP_ */
