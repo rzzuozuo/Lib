e54e721f858c3b978617a0afa3ffde5018d3b70e
Allen Wang <326727955@qq.com>
2021-05-31 12:00:30 +0800

    add start and stop

--- Drivers/I2cPin.hpp
+++ Drivers/I2cPin.hpp
@@ -12,12 +12,35 @@
 
 class I2cPin {
 public:
+
+	enum State{
+		Ok,
+		Error,
+	};
+
 	I2cPin(GPIO_TypeDef  *scl_gpiox, uint16_t scl_pin, GPIO_TypeDef  *sda_gpiox,uint16_t sda_pin);
 
 	void init();
 
 	uint8_t sendBit(uint8_t bit);
 	uint8_t readBit();
+
+	State start(){
+		if((scl->readPin() != Pin::SET) || (sda->readPin() != Pin::SET)){
+			return Error;
+		}
+
+		sda->resetPin();
+		scl->resetPin();
+
+		return Ok;
+	}
+
+	State stop(){
+		scl->setPin();
+		sda->setPin();
+		return Ok;
+	}
 
 	virtual ~I2cPin();
 
