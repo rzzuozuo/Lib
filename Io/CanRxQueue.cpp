8f04372f8efeb5f2480f3a8fcee397e40b1ff0fb
Allen Wang <326727955@qq.com>
2021-08-26 11:37:58 +0800

    删除了 启动接收函数

--- Io/CanRxQueue.cpp
+++ Io/CanRxQueue.cpp
@@ -14,7 +14,6 @@ CanRxQueue::CanRxQueue(Can &can):can(can) {
 void CanRxQueue::run(){
 	osStatus_t status;
 	Can::Msg msg;
-	can.activateNotification(0);
 	for(;;){
 		status = osMessageQueueGet(can.getQueueId(), &msg, NULL, osWaitForever);   // wait for message
 	    if (status == osOK) {
